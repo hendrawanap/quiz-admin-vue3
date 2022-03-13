de85a5e204026560cb96108828a209f85dc883bc
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2022-03-13 15:54:10 +0700

    Adjust to not using top-level await

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from 'vue';
 import makeRouter from './router';
-import store from './store';
+import makeStore from './store';
 import App from './App.vue';
 import './index.css';
 
 const app = createApp(App);
-const router = makeRouter({ store });
-app.use(store);
-app.use(router);
-app.mount('#app');
+makeStore().then((store) => {
+  const router = makeRouter({ store });
+  app.use(store);
+  app.use(router);
+  app.mount('#app');
+});
--- src/store/index.js
+++ src/store/index.js
@@ -3,74 +3,76 @@ import axios from 'axios';
 import { createStore } from 'vuex';
 import initAuth from '../auth';
 
-const apiAddress = import.meta.env.VITE_API_ADDRESS;
-// const savedUserToken = window.localStorage.getItem('user_token') || null;
-const { signInWithEmail, logout, getCurentUserIdToken } = await initAuth();
+export default async () => {
+  const apiAddress = import.meta.env.VITE_API_ADDRESS;
+  // const savedUserToken = window.localStorage.getItem('user_token') || null;
+  const { signInWithEmail, logout, getCurentUserIdToken } = await initAuth();
 
-const store = createStore({
-  state: {
-    isLoggedIn: false,
-    questions: null,
-    isFetchedAll: false,
-  },
-  getters: {
-    getAllQuestions: (state) => state.questions,
-    getQuestionsByTopic: (state) => (topic) => (
-      state.questions.filter((question) => topic === question.topic)
-    ),
-    getQuestionById: (state) => (id) => (
-      id && state.questions ? state.questions.find((question) => id === question.id) : null
-    ),
-  },
-  mutations: {
-    setIsLoggedIn: (state, { isLoggedIn }) => {
-      state.isLoggedIn = isLoggedIn;
+  const store = createStore({
+    state: {
+      isLoggedIn: false,
+      questions: null,
+      isFetchedAll: false,
     },
-    setQuestions: (state, { questions }) => {
-      state.questions = questions;
+    getters: {
+      getAllQuestions: (state) => state.questions,
+      getQuestionsByTopic: (state) => (topic) => (
+        state.questions.filter((question) => topic === question.topic)
+      ),
+      getQuestionById: (state) => (id) => (
+        id && state.questions ? state.questions.find((question) => id === question.id) : null
+      ),
     },
-    setFetchedAll: (state) => {
-      state.isFetchedAll = true;
+    mutations: {
+      setIsLoggedIn: (state, { isLoggedIn }) => {
+        state.isLoggedIn = isLoggedIn;
+      },
+      setQuestions: (state, { questions }) => {
+        state.questions = questions;
+      },
+      setFetchedAll: (state) => {
+        state.isFetchedAll = true;
+      },
+      setUserToken: (state, { userToken }) => {
+        state.userToken = userToken;
+        window.localStorage.setItem('user_token', userToken);
+      },
+      deleteUserToken: (state) => {
+        state.userToken = null;
+        window.localStorage.removeItem('user_token');
+      },
     },
-    setUserToken: (state, { userToken }) => {
-      state.userToken = userToken;
-      window.localStorage.setItem('user_token', userToken);
-    },
-    deleteUserToken: (state) => {
-      state.userToken = null;
-      window.localStorage.removeItem('user_token');
-    },
-  },
-  actions: {
-    fetchQuestions: async ({ commit }) => {
-      const userToken = await getCurentUserIdToken();
-      const { data } = await axios.get(`${apiAddress}/questions?token=${userToken}`);
-      commit('setQuestions', { questions: data });
-      commit('setFetchedAll');
-    },
-    fetchQuestionById: async ({ commit }, { id }) => {
-      const { data } = await axios.get(`${apiAddress}/questions/${id}`);
-      commit('setQuestions', { questions: [data] });
-    },
-    checkIsLoggedIn: async ({ commit }) => {
-      try {
+    actions: {
+      fetchQuestions: async ({ commit }) => {
         const userToken = await getCurentUserIdToken();
+        const { data } = await axios.get(`${apiAddress}/questions?token=${userToken}`);
+        commit('setQuestions', { questions: data });
+        commit('setFetchedAll');
+      },
+      fetchQuestionById: async ({ commit }, { id }) => {
+        const { data } = await axios.get(`${apiAddress}/questions/${id}`);
+        commit('setQuestions', { questions: [data] });
+      },
+      checkIsLoggedIn: async ({ commit }) => {
+        try {
+          const userToken = await getCurentUserIdToken();
+          commit('setIsLoggedIn', { isLoggedIn: true });
+        } catch (error) {
+          commit('setIsLoggedIn', { isLoggedIn: false });
+        }
+      },
+      signInWithEmail: async ({ commit }, { email, password }) => {
+        const userToken = await signInWithEmail(email, password);
+        // commit('setUserToken', { userToken });
         commit('setIsLoggedIn', { isLoggedIn: true });
-      } catch (error) {
+      },
+      logout: async ({ commit }) => {
+        await logout();
+        // commit('deleteUserToken');
         commit('setIsLoggedIn', { isLoggedIn: false });
-      }
+      },
     },
-    signInWithEmail: async ({ commit }, { email, password }) => {
-      const userToken = await signInWithEmail(email, password);
-      // commit('setUserToken', { userToken });
-      commit('setIsLoggedIn', { isLoggedIn: true });
-    },
-    logout: async ({ commit }) => {
-      await logout();
-      // commit('deleteUserToken');
-      commit('setIsLoggedIn', { isLoggedIn: false });
-    },
-  },
-});
+  });
 
-export default store;
+  return store;
+};
